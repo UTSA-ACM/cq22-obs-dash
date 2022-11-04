c46e8b95af33aa61b0f8ea5b7490d06ff1289a3a
Liam Murray <liam@murray.to>
2022-11-04 15:30:24 -0500

    Add Time / Resources

--- src/pages/obs/index.tsx
+++ src/pages/obs/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import Snowfall from "react-snowfall";
 import Head from "next/head";
+import Clock from "react-live-clock";
 
 import NowEventItem from "../../components/NowEventItem";
 import SoonEventItem from "../../components/SoonEventItem";
@@ -37,8 +39,16 @@ const OBS: NextPage = () => {
 			</Head>
 			{showFallEffect ? <Snowfall speed={[0.1, 0.2]} /> : null}
 			<div className="grid h-screen w-full grid-cols-2 overflow-y-auto bg-cq-lightgreen">
+				<Image
+					src="/img/clipart/tree.svg"
+					className="absolute bottom-0 right-10 z-0"
+					alt="CQ Tree"
+					width={300}
+					height={300}
+				/>
 				<div className="z-10 flex flex-col items-start justify-center p-[10px] pl-[30px]">
 					<h1 className="header-gradient font-sans text-6xl font-extrabold">CodeQuantum 2022</h1>
+					<br />
 					<br />
 					<div className="flex w-full flex-col justify-center">
 						<div>
@@ -54,9 +64,30 @@ const OBS: NextPage = () => {
 						</div>
 						<div>
 							<h3 className="header-gradient flex flex-col text-xl font-extrabold">Up Next:</h3>
-							{soonEvents.length > 0
-								? soonEvents.map((event) => <SoonEventItem key={Math.random()} {...event} />)
-								: null}
+							{soonEvents.length > 0 ? (
+								soonEvents.map((event) => <SoonEventItem key={Math.random()} {...event} />)
+							) : (
+								<p className="break-normal text-xl font-extrabold text-white">🍁 Hacking 2?</p>
+							)}
+							<br />
+							<br />
+						</div>
+
+						<div>
+							<h3 className="header-gradient flex flex-col text-xl font-extrabold">Resources:</h3>
+							<h2 className="break-normal text-xl font-extrabold text-white">guide.cqhacks.org</h2>
+							<h2 className="break-normal text-xl font-extrabold text-white">
+								go.cqhacks.org/schedule
+							</h2>
+							<br />
+						</div>
+						<br />
+						<div className="flex h-[95px] max-w-[580px] items-center justify-center rounded-lg bg-white p-[5px]">
+							<Clock
+								className="header-gradient font-sans text-6xl font-extrabold"
+								format="h:mm A"
+								ticking={true}
+							/>
 						</div>
 					</div>
 				</div>
